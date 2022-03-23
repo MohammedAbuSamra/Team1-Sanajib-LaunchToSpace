a41c9c1afa5db19c8f17d59fac83edaf8d4a99e5
Mahmoud Hammad <0597694093mahmoud@gmail.com>
2022-03-23 23:37:32 +0200

    added the functionality to display all launches relates #14

--- JS/launches.js
+++ JS/launches.js
@@ -63,3 +63,117 @@ fetch('https://api.spacexdata.com/v4/launches/latest')
 // let hours = document.querySelector('.hours');
 // let minutes = document.querySelector('.minutes');
 // let seconds = document.querySelector('.seconds');
+
+fetch('https://api.spacexdata.com/v4/launches/')
+.then(res => res.json())
+.then(data => {
+  const allLaunches = document.querySelector('.all-launches-container');
+
+
+   // data.forEach {
+   //   cardContainer.appendChild(cardsRender(data[i]))
+   // }
+  data.forEach((item) => {
+      allLaunches.appendChild(cardsRender(item));
+      // cardsContainer.appendChild(card);
+    });
+})
+.catch(err => console.log('error', err));
+
+
+function cardsRender(incomingData) {
+
+  const card = document.createElement("div");
+  card.setAttribute('class', 'trips-card'); 
+  // ** Start creation of an images div
+  const tripImgDiv = document.createElement('div');
+  const tripImg = document.createElement('img');
+  tripImg.setAttribute('src', `${incomingData.links.patch.small}`);
+  tripImg.setAttribute('alt', `${incomingData.name}`);
+  tripImg.setAttribute('referrerpolicy', 'no-referrer')
+  tripImgDiv.setAttribute('class', 'trip-img');
+  tripImgDiv.setAttribute('class', 'trip-img');
+  tripImgDiv.appendChild(tripImg)
+  card.appendChild(tripImgDiv)
+  // !! End creation of an images div
+
+  // ** Start creation of an trip information div
+
+    const tripInformationDiv = document.createElement('div');
+    tripInformationDiv.setAttribute('class', 'trip-information');
+
+    // ** start creation of heading title
+    const headingTitle = document.createElement('h1');
+    headingTitle.textContent = `${incomingData.name}`;
+    headingTitle.setAttribute('class', 'trip-title');
+    card.appendChild(tripInformationDiv)
+    
+
+    // ** start creation of trip date div
+      const tripDateDiv = document.createElement('div');
+      tripDateDiv.setAttribute('class', 'trip-date');
+      tripDateDiv.appendChild(headingTitle)
+      const clockIcon = document.createElement('i');
+      clockIcon.classList.add('fa-regular', 'fa-clock');
+      const datePara = document.createElement('p');
+      
+      datePara.textContent = `${incomingData.date_local.slice(0, 10)}`;
+
+      tripDateDiv.appendChild(clockIcon);
+      tripDateDiv.appendChild(datePara);
+      tripInformationDiv.appendChild(tripDateDiv)
+
+    // !! End creation of trip date div
+
+    // ** start creation of trip description div
+    
+    const tripDescriptionDiv = document.createElement('div');
+    tripDescriptionDiv.setAttribute('class', 'trip-description')
+    const descriptionPara = document.createElement('p');
+    descriptionPara.textContent = `${incomingData.details}`;
+    tripDescriptionDiv.appendChild(descriptionPara);
+    tripInformationDiv.appendChild(tripDescriptionDiv);
+    
+    // !! End creation of trip description div
+
+      // ** start creation of trip buttons div
+        
+        const buttonsDiv = document.createElement('div');
+        buttonsDiv.setAttribute('class', 'trip-btns');
+
+        // ?? create youtube button
+        const youtubeBtn = document.createElement('a');
+        youtubeBtn.textContent = 'youtube';
+        youtubeBtn.setAttribute('class', 'youtube-btn');
+        youtubeBtn.setAttribute('href', `${incomingData.links.webcast}`);
+        youtubeBtn.setAttribute('target', '_blank');
+        // ?? create read more button
+        const readMoreBtn = document.createElement('a');
+        readMoreBtn.textContent = 'read more ';
+        readMoreBtn.setAttribute('class', 'more');
+        readMoreBtn.setAttribute('href', `${incomingData.links.article}`);
+        readMoreBtn.setAttribute('target', '_blank');
+
+        const rightArrowIcon = document.createElement('i');
+        rightArrowIcon.classList.add('fa-solid', 'fa-chevron-right');
+        readMoreBtn.appendChild(rightArrowIcon);
+        buttonsDiv.appendChild(youtubeBtn);
+        buttonsDiv.appendChild(readMoreBtn);
+        tripInformationDiv.appendChild(buttonsDiv)
+      
+      // !! End creation of trip buttons div
+      
+      // ** Start creation of heart icon div
+        const heartDiv = document.createElement('div');
+        heartDiv.setAttribute('class', 'heart')
+        const heartIcon = document.createElement('i');
+        heartIcon.classList.add('fa-regular', 'fa-heart');
+        heartDiv.appendChild(heartIcon);
+        tripInformationDiv.appendChild(heartDiv)
+      // !! End creation of heart icon div
+
+    // ** End creation of trip date div
+  
+  // !! End creation of an trip information div
+  return card;
+}
